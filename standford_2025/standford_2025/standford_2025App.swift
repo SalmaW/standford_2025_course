3acf5bef30a79ee93d63dd03910b9e4a5dd55479
Salma <salmamaarouf00@gmail.com>
2026-02-08 00:23:10 +0200

    initial commet

---
+++ standford_2025/standford_2025/standford_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  standford_2025App.swift
+//  standford_2025
+//
+//  Created by Wael Maarouf on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct standford_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
